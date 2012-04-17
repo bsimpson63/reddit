f64aa868b7c506ea7d0e80099673b2dc6f0dd4ad
bsimpson63 <bsimpson63@gmail.com>
2012-04-16 17:00:52 -0700

    Add _on_commit hook for tdb_cassandra.ThingBase.

--- r2/r2/lib/db/tdb_cassandra.py
+++ r2/r2/lib/db/tdb_cassandra.py
@@ -605,6 +605,8 @@ class ThingBase(object):
 
         if not self._committed:
             self._on_create()
+        else:
+            self._on_commit()
 
         self._committed = True
 
@@ -718,6 +720,10 @@ class ThingBase(object):
         """A hook executed on creation, good for creation of static
            Views. Subclasses should call their parents' hook(s) as
            well"""
+        pass
+
+    def _on_commit(self):
+        """Executed on _commit other than creation."""
         pass
 
     @classmethod
